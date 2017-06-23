c7894bab4cdfb7c926ae24753a2fc1301d391513
gordon_br <jonathasalves20@gmail.com>
2017-06-22 22:04:47 -0300

    Project Setup

---
+++ exitWebApp/src/main/java/com/john/main/Main.java
@@ -0,0 +1,11 @@
+package com.john.main;
+
+/**
+ * Created by jonathasalves on 22/06/2017.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.print("it's alive");
+    }
+}
