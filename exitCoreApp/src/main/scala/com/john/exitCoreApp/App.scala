16e2a9880e111a0662530375fd827b8312c636ae
gordon_br <jonathasalves20@gmail.com>
2017-08-07 18:44:21 -0300

    Project Setup

--- exitCoreApp/src/main/scala/com/john/exitCoreApp/App.scala
+++ exitCoreApp/src/main/scala/com/john/exitCoreApp/App.scala
@@ -1,55 +1,127 @@
 package com.john.exitCoreApp
 
+import java.util.concurrent.Future
 import java.util.regex.Pattern
 
+import kafka.serializer.StringDecoder
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
+class IngesterParams(val topicsSet: Set[String], val brokers: String)
+
 /**
- * @author ${user.name}
- */
+  * @author Jonathas Alves
+  */
 object App {
 
-  def loadURL(path:String, sc:SparkContext) : RDD[String] = {
+  def loadURL(path: String, sc: SparkContext): RDD[String] = {
     sc.textFile(path)
   }
-  
-  def splitURL(urlsRDD:RDD[String]) = {
+
+  def splitURL(urlsRDD: DStream[String]): DStream[(String, String, String, String, String)] = {
     val regEx: String = "^(\\d{1,3}\\.){3}\\d{1,3} - - \\[(\\d){2}/(\\w){3}/(\\d){4}:(\\d){2}:(\\d){2}:(\\d){2} -0400\\]" +
       " (\".+\") (\\d)+ (\\d)+?"
 
     val pattern = Pattern.compile("([\\S\\.]+) - - (\\[.*?\\]) (\".+\") (\\d+) (\\d+)")
 
+    println("here here")
+
     val groupsRDD = urlsRDD.map(line => pattern.matcher(line)).filter(pat => pat.find()).map(pat =>
       (pat.group(1), pat.group(2), pat.group(3), pat.group(4), pat.group(5)))
 
-    //groupsRDD.collect().foreach(group => println(String.format("%s %s %s %s %s", group._1, group._2, group._3, group._4, group._5)))
-    println("TOTAL COUNT " + groupsRDD.count())
+    groupsRDD
+  }
+
+  def kafkaStream(ssc: StreamingContext, ingesterParams: IngesterParams) = {
+
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> ingesterParams.brokers, "auto.offset.reset" -> "smallest")
+    val directKafkaStream = KafkaUtils.createDirectStream[
+      String, String, StringDecoder, StringDecoder](ssc, kafkaParams, ingesterParams.topicsSet)
+
+    val lines = directKafkaStream.map(_._2)
+    lines.count().print()
+    val groupsRDD = splitURL(lines)
+
+    val stringStreamRDD = groupsRDD.map(groups =>
+      String.format("%s == %s == %s == %s == %s", groups._1, groups._2, groups._3, groups._4, groups._5))
+
+    stringStreamRDD.foreachRDD(rdd => rdd.collect().foreach(println))
+
+    sparkToKafka(stringStreamRDD)
+  }
+
+  def sparkToKafka(stream: DStream[String]) = {
+
+    stream.foreachRDD(rdd => {
+
+      rdd.foreachPartition(partition => {
+        val topic = "exit_processed_topic"
+
+        val props = new java.util.Properties()
+        props.put("bootstrap.servers", "localhost:9092")
+        props.put("client.id", "KafkaProducer")
+        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        val producer = new KafkaProducer[Integer, String](props)
+
+        partition.foreach(line => {
+
+          val record = new ProducerRecord[Integer, String](topic, 1, line.toString)
+          producer.send(record)
+
+        })
+
+        producer.close()
+      })
+    })
+
+  }
+
+  def defSsc(ingesterParams: IngesterParams): StreamingContext = {
+
+    val conf = new SparkConf().setAppName("simple-ingester").setMaster("local[*]")
+    val ssc = new StreamingContext(conf, Seconds(1))
+    kafkaStream(ssc, ingesterParams)
+    ssc
+  }
+
+  def main(args: Array[String]) {
+
+    println("it's alive spark")
+    val ingesterParams = new IngesterParams(Set("exit_dma_topic"), "localhost:9092")
+    val ssc = defSsc(ingesterParams)
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop()
   }
 
 
-  def matchFunction() = {
-    val text = "199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] \"GET /history/apollo/ HTTP/1.0\" 200 6245"
-    val regEx: String = "^(\\d{1,3}\\.){3}\\d{1,3} - - \\[(\\d){2}/(\\w){3}/(\\d){4}:(\\d){2}:(\\d){2}:(\\d){2} -0400\\]" +
-      " (\".+\") (\\d)+ (\\d)+?"
-    val pattern = Pattern.compile("([\\d\\.]+) - - (\\[.*?\\]) (\".+\") (\\d+) (\\d+)")
+  def sparkToKafka2() = {
 
-    System.out.println(text.matches(regEx))
-    val pat = pattern.matcher(text)
-    if(pat.find())
-      System.out.println(pat.group(1) + " " + pat.group(2) + " " + pat.group(3) + " " + pat.group(4) + " " + pat.group(5))
-  }
+    val topic = "exit_processed_topic"
 
-  
-  def main(args : Array[String]) {
+    val props = new java.util.Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("client.id", "KafkaProducer")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-    matchFunction()
-    val sconf = new SparkConf().setMaster("local").setAppName("coreApp")
-    val sc = new SparkContext(sconf)
 
-    val urlRDD = loadURL("/Users/jonathasalves/Programming/nasa_access_log", sc)
+    val producer = new KafkaProducer[Integer, String](props)
+    val record = new ProducerRecord[Integer, String](topic, 1, "test")
+    val metaF: Future[RecordMetadata] = producer.send(record)
+    val meta = metaF.get()
+    // blocking!
+    val msgLog =
+      s"""|offset    = ${meta.offset()}|partition = ${meta.partition()}|topic     = ${meta.topic()}""".stripMargin
+    println(msgLog)
+    Thread.sleep(5)
+    producer.close()
 
-    splitURL(urlRDD)
   }
 
 }
---
+++ exitCoreApp/src/main/scala/com/john/fakeProducers/KafkaFakeProducer.scala
@@ -0,0 +1,83 @@
+package com.john.fakeProducers
+
+import java.util.concurrent.Future
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
+
+import scala.io.Source
+
+/**
+  * Created by jonathasalves on 26/06/2017.
+  */
+object KafkaFakeProducer {
+
+  //val topic = util.Try(args(0)).getOrElse("test")
+  val topic = "exit_dma_topic"
+  val file = "/Users/jonathasalves/Programming/nasa_access_log"
+
+  def main(args: Array[String]): Unit = {
+    println(s"Connecting to topic: $topic")
+    while (true) {
+      val iterator = readFiles(file)
+      pushData(iterator)
+      Thread.sleep(3000)
+    }
+  }
+
+  //  val props = new java.util.Properties()
+  //  props.put("bootstrap.servers", "localhost:9092")
+  //  props.put("client.id", "KafkaProducer")
+  //  props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
+  //  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  //
+  //  val producer = new KafkaProducer[Integer, String](props)
+  //
+  //  import org.apache.kafka.clients.producer.ProducerRecord
+  //
+  //  val polish = java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss")
+  //
+  //  val lines = readFiles(file)
+  //  var n = 3
+  //  while(n < 100000) {
+  //    val now = java.time.LocalDateTime.now().format(polish)
+  //    val record = new ProducerRecord[Integer, String](topic, 1, n.toString)
+  //    val metaF: Future[RecordMetadata] = producer.send(record)
+  //    val meta = metaF.get()
+  //    // blocking!
+  //    val msgLog =
+  //    s"""|offset    = ${meta.offset()}|partition = ${meta.partition()}|topic     = ${meta.topic()}""".stripMargin
+  //    println(msgLog)
+  //    Thread.sleep(10)
+  //    n += 3
+  //  }
+  //
+  //  producer.close()
+
+  def readFiles(file: String): Iterator[String] = {
+    for (line <- Source.fromFile(file).getLines()) yield line
+  }
+
+  def pushData(listData: Iterator[String]) = {
+
+    val props = new java.util.Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("client.id", "KafkaProducer")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[Integer, String](props)
+
+    for (line <- listData) {
+      val record = new ProducerRecord[Integer, String](topic, 1, line)
+      val metaF: Future[RecordMetadata] = producer.send(record)
+      val meta = metaF.get()
+      // blocking!
+      val msgLog =
+        s"""|offset    = ${meta.offset()}|partition = ${meta.partition()}|topic     = ${meta.topic()}""".stripMargin
+      println(msgLog)
+      Thread.sleep(100)
+    }
+
+    producer.close()
+  }
+}
